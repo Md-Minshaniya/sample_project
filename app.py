aa796561bf70b43cc6781dc21ef93a6f52e69cb8
Md-Minshaniya <minish4279niya@gmail.com>
2025-08-02 11:39:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
-app=Flask(__name__)
+
+app = Flask(__name__)
+
 @app.route("/")
 def index():
-      return "Welcome to MRECW"
-if __name__=="main":
-      app.run(host='0.0.0.0',port=10000)
+    return "Welcome to MRECW"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=10000)
